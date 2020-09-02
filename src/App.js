89ddf2e20f6714dabc12677b97827767d836f9c1
snikhilprdxn <nikhils>
2020-09-02 13:18:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import VideoRecorder from 'react-video-recorder';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <VideoRecorder
+          style={{
+            height: '30vw',
+            video: '80%',
+            width: '60%',
+            margin: '0 auto',
+            margin: '50px 0 0 0'
+          }}
+
+          onRecordingComplete = {(videoBlob) => {                        
+            // const src = window.URL.createObjectURL(videoBlob);
+            // console.log(src);
+            console.log('videoBlob', videoBlob)
+            const url = URL.createObjectURL(videoBlob);
+            console.log(url);
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = url;
+            console.log(a.href);
+            a.download = new Date() +".webm";
+            a.click();
+            window.URL.revokeObjectURL(url);
+          }} 
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
